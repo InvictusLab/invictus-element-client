b50db3b7cc8c4d1f00bfdae79e7f2e8db56eed25
Krystal <jian.yang@oebiotech.com>
2024-03-03 15:33:31 +0800

    feat(dragon): configure eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,13 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
+    // Vue语法的ESLint插件
+    'plugin:vue/vue3-recommended',
+    // 继承Vue官方提供的ESLint标准配置，ESLint+TypeScript、Lint+Vue、Lint三个插件的集合与配置
+    '@vue/eslint-config-standard-with-typescript',
+    // 继承Vue官方提供的ESLintPrettier标准配置
+    '@vue/eslint-config-prettier'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
